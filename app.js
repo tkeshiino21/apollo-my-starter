5745e04d72d0b327b1f0d4a371d8b589e5474d4d
inouejoushi <takeshitoras.79@gmail.com>
2020-03-30 21:47:28 +0900

    make it better

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { users } = require("./users");
+const path = require("path");
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type User {
+    id: Int
+    name: String
+    age: Int
+    created_date: String
+  }
+  type Query {
+    users: [User]
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    users: () => users
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 4000;
+
+console.log(process.env);
+
+app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+// app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
---
+++ users.js
@@ -0,0 +1,21 @@
+exports.users = [
+  {
+    id: 1,
+    name: "Zonomaa",
+    age: 25,
+    created_date: "2019-04-05 01:35:25.000"
+  },
+  {
+    id: 2,
+    name: "Masa",
+    age: 28,
+    created_date: "2019-04-10 08:23:51.000"
+  },
+  {
+    id: 3,
+    name: "Ms",
+    age: 30,
+    created_date: "2019-04-10 08:32:09.000"
+  }
+  // ... 適当にいくつか作ってみましょう
+];
